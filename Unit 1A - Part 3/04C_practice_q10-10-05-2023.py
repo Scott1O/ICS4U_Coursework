52fcf41845f2d110fb35e6d521b3f6bc8e12ba23
Scott O <regigigas@rogers.com>
2023-10-05 14:35:36 -0400

    Finished question 10.

--- Unit 1A - Part 3/04C_practice_q10-10-05-2023.py
+++ Unit 1A - Part 3/04C_practice_q10-10-05-2023.py
@@ -1,3 +1,28 @@
 # Scott O'Hara 
 # Date: 10-05-2023
 # 04C Practice Question 10
+
+# input
+num = int(input("Enter a number to find it's prime factors: "))
+largest = 1
+num_holder = num
+
+# processing
+if num % 2 == 0:
+    largest = 2
+    # while loop to divide by 2 as many times
+    while num % 2 == 0:
+        num //= 2
+# end of while loop - number should now be odd
+
+factor = 3
+while num != 1:
+    if num % factor == 0:
+        num //= factor
+        largest = factor
+    else:
+        factor += 2
+# end of while loop
+
+# output
+print(f"The largest prime factor of {num_holder} is {largest}.")
