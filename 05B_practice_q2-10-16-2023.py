47354e156303485598a2e1d8eeebf44d429bee11
Scott1O <146479808+Scott1O@users.noreply.github.com>
2023-10-16 17:18:17 -0400

    Add files via upload
    added the first three questions from Unit 1B through upload

---
+++ 05B_practice_q2-10-16-2023.py
@@ -0,0 +1,25 @@
+# Scott O'Hara
+# Date: 10-16-2023
+# 05B Practice Question 2
+
+def palindrome_checker(user_input):
+    '''
+    checks if an inputted string is a palindrome or not
+
+    argument:
+        user_input: takes the users input, string
+    
+    return:
+        True or False whether or not the input is a palindrome
+    '''
+    reversed_input = ""
+    for letter in reversed(user_input):
+        reversed_input += letter
+    palindrome = bool(user_input == reversed_input)
+    print(reversed_input)
+
+    return palindrome
+# end of function
+
+word = input("Enter a word: ")
+print(f"Your word, {word}, is a palindrome: {palindrome_checker(word)}")
