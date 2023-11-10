5463b8b01f9eb92842157db30736a998bae6b6f6
Scott O <sskohara@icloud.com>
2023-11-09 21:01:32 -0500

    Worked in question 2 for GP. Not sure if it works since it is too inefficient and takes too long.

--- GPQs/gp-q2.py
+++ GPQs/gp-q2.py
@@ -7,11 +7,12 @@ def factors(num):
     return factors_list
 
 amicable_pairs = {}
-for i in range(1, 10001):
-    for l in range(1,10001):
+for i in range(2, 10001):
+    for l in range(2,10001):
         suml = sum(factors(l))
         sumi = sum(factors(i))
-        if sumi == suml:
+        if sumi == l and suml == i and i != l and (i not in amicable_pairs and l not in amicable_pairs):
             amicable_pairs[i] = l 
 
 print(amicable_pairs)
+print((sum(amicable_pairs.values()) + sum(amicable_pairs.keys())))
